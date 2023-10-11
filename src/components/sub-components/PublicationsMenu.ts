670d24cd4024a7dfcc4d6765fd7501840b9f0462
stefanolami <stefanolami90@gmail.com>
2023-10-11 13:24:53 +0300

    1

--- src/components/sub-components/PublicationsMenu.ts
+++ src/components/sub-components/PublicationsMenu.ts
@@ -13,9 +13,9 @@ export class PublicationsMenu extends TailwindElement(style) {
             <div class="max-w-7xl mx-auto select-none">
                 <h1 class="font-unna-700 text-[20px] md:text-[39px] xl:text-[50px] md:mb-12 xl:mb-20 text-center">PUBLICATIONS</h1>
                 <div class="flex mx-5 my-5 flex-row flex-wrap justify-between md:justify-center md:mx-auto">                                    
-                    <div class="publications-menu-divs">Analyses & Commentaries</div>
-                    <div class="publications-menu-divs">The Covid of Things</div>
-                    <div class="publications-menu-divs">Predictability in Politics</div>
+                    <div class="publications-menu-divs hover:bg-[#1D2DAF]/90">Analyses & Commentaries</div>
+                    <div class="publications-menu-divs hover:bg-[#1D2DAF]/90">The Covid of Things</div>
+                    <div class="publications-menu-divs hover:bg-[#1D2DAF]/90">Predictability in Politics</div>
                     <div class="w-[48%] md:w-[24%] relative group">
                         <div class="border-2 border-primary rounded-full md:rounded-none md:rounded-br-3xl w-full font-jose-600 md:font-normal text-primary-dark2 md:text-white text-[9px] md:text-base leading-[9px] text-center  my-1 md:mx-[1.5px] xl:mx-[1px] py-[7px] md:bg-primary xl:group-hover:bg-[#1D2DAF]/90 cursor-pointer">Time & Place Newsletters</div>
                         <div class="absolute w-full top-[42px] left-0 hidden mt-2 xl:group-hover:block z-50">
